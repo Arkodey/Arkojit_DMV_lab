7ba9ba4f521a55e5611605abaa1bb17441aa5daf
Arkodey <151497030+Arkodey@users.noreply.github.com>
2026-02-24 11:37:00 +0530

    Add files via upload

---
+++ pieStatic.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Data for the pie chart
+slices = [7, 2, 8, 13]
+activities = ['Sleeping', 'Eating', 'Working', 'Playing']
+cols = ['c', 'm', 'r', 'b']
+
+plt.pie(slices,
+        labels=activities,
+        colors=cols,
+        startangle=90,
+        autopct='%1.1f%%')
+
+plt.title('My Daily Activities')
+
+plt.show()
